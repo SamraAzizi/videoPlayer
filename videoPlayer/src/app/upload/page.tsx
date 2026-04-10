9c30ff6719b122c9fb7c6822bade576ee5e77b31
SamraAzizi <samarraazizi@gmail.com>
2026-04-10 17:24:51 +0500

    video player

---
+++ videoPlayer/src/app/upload/page.tsx
@@ -0,0 +1,8 @@
+import VideoUpload from "@/components/video/VideoUpload";
+
+export default function UploadPage() {
+    return <div>
+        <h1>Upload Videos</h1>
+        <VideoUpload />
+    </div>
+}
